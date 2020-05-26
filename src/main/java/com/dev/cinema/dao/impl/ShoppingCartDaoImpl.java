0840f1a21b4212549f383dc006f0b63e986de7d2
ihor <krohmal.igor777@gmail.com>
2020-05-26 13:15:30 +0300

    implemented solution 27.3

---
+++ src/main/java/com/dev/cinema/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,84 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.ShoppingCartDao;
+import com.dev.cinema.exception.DataProcessingException;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.ShoppingCart;
+import com.dev.cinema.model.User;
+import com.dev.cinema.util.HibernateUtil;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+    private static final Logger LOGGER = Logger.getLogger(ShoppingCartDaoImpl.class);
+
+    @Override
+    public ShoppingCart add(ShoppingCart shoppingCart) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            Long shoppingCartId = (Long) session.save(shoppingCart);
+            transaction.commit();
+            LOGGER.info(String.format("Shopping cart with id - %s successfully added.",
+                    shoppingCartId));
+            return shoppingCart;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert Shopping cart entity", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<ShoppingCart> criteriaQuery = criteriaBuilder
+                    .createQuery(ShoppingCart.class);
+            Root<ShoppingCart> root = criteriaQuery.from(ShoppingCart.class);
+            root.fetch("tickets", JoinType.LEFT);
+            Predicate predicate = criteriaBuilder.equal(root.get("user"), user.getId());
+            criteriaQuery.where(predicate);
+            return session.createQuery(criteriaQuery).getSingleResult();
+        } catch (Exception e) {
+            throw new DataProcessingException("Error retrieving Shopping cart", e);
+        }
+    }
+
+    @Override
+    public void update(ShoppingCart shoppingCart) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.update(shoppingCart);
+            transaction.commit();
+            LOGGER.info(String.format("Shopping cart with id - %s successfully updated.",
+                    shoppingCart.getId()));
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't update Shopping cart entity", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
