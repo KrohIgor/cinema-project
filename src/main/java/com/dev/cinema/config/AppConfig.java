16a523b6a7546fbe28782a56b9c8a9d61ef82f1a
ihor <krohmal.igor777@gmail.com>
2020-06-09 19:18:33 +0300

    changed factoryBean.setPackagesToScan()

--- src/main/java/com/dev/cinema/config/AppConfig.java
+++ src/main/java/com/dev/cinema/config/AppConfig.java
@@ -1,12 +1,5 @@
 package com.dev.cinema.config;
 
-import com.dev.cinema.model.CinemaHall;
-import com.dev.cinema.model.Movie;
-import com.dev.cinema.model.MovieSession;
-import com.dev.cinema.model.Order;
-import com.dev.cinema.model.ShoppingCart;
-import com.dev.cinema.model.Ticket;
-import com.dev.cinema.model.User;
 import java.util.Properties;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -55,8 +48,7 @@ public class AppConfig {
         properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
 
         factoryBean.setHibernateProperties(properties);
-        factoryBean.setAnnotatedClasses(CinemaHall.class, Movie.class, MovieSession.class,
-                Order.class, ShoppingCart.class, Ticket.class, User.class);
+        factoryBean.setPackagesToScan("com.dev.cinema.model");
         return factoryBean;
     }
 }
